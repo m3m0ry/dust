cadfde26d55d1c00187b79a977b5e9e4b11b6822
Jan Hönig <jan.hoenig@fau.de>
2020-06-05 14:25:53 +0200

    working example

--- source/app.d
+++ source/app.d
@@ -12,14 +12,15 @@ string mine()
 	auto c = sym[2];
 	auto n = c += a + b + 1;
 	auto n2 = new Block([n, new Statement(new FunctionCall("writeln", [sym[2]]))]);
-	auto ret = format!"%s"(n2);
+	string ret = format!"%s"(n2);
+  return ret;
 	
-	auto fs = fields!real(["A", "B"], 2, [128,128]);
-	auto A = fs[0];
-	auto B = fs[1];
-	auto jacobi = B[0] = 1./4. *(A[1,0] + A[0,1] + A[-1,0] + A[0,-1]);
+	//auto fs = fields!real(["A", "B"], 2, [128,128]);
+	//auto A = fs[0];
+	//auto B = fs[1];
+	//auto jacobi = B[0] = 1./4. *(A[1,0] + A[0,1] + A[-1,0] + A[0,-1]);
 
-	return jacobi.to!string;
+	//return jacobi.to!string;
 }
 
 void main()
@@ -29,5 +30,5 @@ void main()
 	real c = 3;
 	enum string s = mine;
 	writeln(s);
-	//mixin(s);
+	mixin(s);
 }
